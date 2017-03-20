368c2ae10dbf09acad6d865818f903d17393b525
Dave Kruger <dave@myria.co.za>
2017-03-20 11:27:35 +0200

    added new style base files and old code

---
+++ tachyonic/netrino_ui/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""Netrino UI"""
+
+from tachyonic_netrino_ui import metadata
+
+
+__version__ = metadata.version
+__author__ = metadata.authors[0]
+__license__ = metadata.license
+__copyright__ = metadata.copyright
